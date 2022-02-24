a1bb192994c5c155d0f728597a16f4b7208b5f33
Melvin <melvinsh@users.noreply.github.com>
2022-02-24 15:13:30 +0100

    Create asns.go

---
+++ asns.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+)
+
+func main() {
+	var matches []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		out := find(scanner.Text())
+
+		if out != "" {
+			matches = append(matches, out)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+
+	for _, match := range unique(matches) {
+		fmt.Println(match)
+	}
+}
+
+func find(ip string) string {
+	var re = regexp.MustCompile(`(?m)(?:\n)(\d*)`)
+	var str = whois(ip)
+
+	out := re.FindStringSubmatch(str)[1]
+	return out
+}
+
+func whois(ip string) string {
+	command := fmt.Sprintf("whois -h v4.whois.cymru.com \"  -v %s\"", ip)
+
+	out, err := exec.Command("bash", "-c", command).Output()
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	return string(out[:])
+}
+
+func unique(strSlice []string) []string {
+	keys := make(map[string]bool)
+	list := []string{}
+	for _, entry := range strSlice {
+		if _, value := keys[entry]; !value {
+			keys[entry] = true
+			list = append(list, entry)
+		}
+	}
+	return list
+}
